bb82302b416aaac966f9226fea1a88ecab578f7e
Maxime Bouthors <maxbouthors@gmail.com>
2019-12-13 11:13:34 +0100

    dqn v2

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from keras.layers import Dense, Activation, Flatten, Convolution2D, Permute, Max
 import keras.backend.common as common
 
 
-def model(input_shape, nb_actions, version : str):
+def model(input_shape, nb_actions, version: str):
 
     if version == "v1":
         return model_v1(input_shape, nb_actions)
@@ -45,7 +45,7 @@ def model_v1(input_shape, nb_actions):
 
 
 def model_v2(input_shape, nb_actions):
-    
+
     model = Sequential()
     if common.image_dim_ordering() == 'tf':
         # (width, height, channels)
@@ -58,15 +58,13 @@ def model_v2(input_shape, nb_actions):
     model.add(Convolution2D(32, (5, 5), strides=(3, 3)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Convolution2D(64, (4, 4), strides=(2, 2)))
+    model.add(Convolution2D(64, (3, 3), strides=(2, 2)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
     model.add(Dense(512))
     model.add(Activation('relu'))
-    model.add(Dense(50))
-    model.add(Activation('relu'))
-    model.add(Dense(30))
+    model.add(Dense(150))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
     # model.add(Activation('linear'))
