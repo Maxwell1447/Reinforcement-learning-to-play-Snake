5ec3ab60cfe67029750f3eb3897a36db2086429f
Maxime Bouthors <maxbouthors@gmail.com>
2020-01-08 23:13:36 +0100

    v5 added

--- dqn/models.py
+++ dqn/models.py
@@ -78,19 +78,20 @@ def model_v3(input_shape, nb_actions):
         model.add(Permute((1, 2, 3), input_shape=input_shape))
     else:
         raise RuntimeError('Unknown image_dim_ordering.')
-    model.add(Convolution2D(32, (5, 5), strides=(3, 3)))
+    model.add(Convolution2D(10, (5, 5), strides=(1, 1)))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Flatten())
-    model.add(Dense(512))
+    model.add(Dense(350))
     model.add(Activation('relu'))
-    model.add(Dense(40))
+    model.add(Dense(80))
     model.add(Activation('relu'))
     model.add(Dense(nb_actions))
-    #model.add(Activation('linear'))
+    model.add(Activation('linear'))
     print(model.summary())
 
     return model
+
 
 def model_v4(input_shape, nb_actions):
 
@@ -118,3 +119,36 @@ def model_v4(input_shape, nb_actions):
     print(model.summary())
 
     return model
+
+
+def model_v5(input_shape, nb_actions):
+
+    model = Sequential()
+    if common.image_dim_ordering() == 'tf':
+        # (width, height, channels)
+        model.add(Permute((2, 3, 1), input_shape=input_shape))
+    elif common.image_dim_ordering() == 'th':
+        # (channels, width, height)
+        model.add(Permute((1, 2, 3), input_shape=input_shape))
+    else:
+        raise RuntimeError('Unknown image_dim_ordering.')
+    model.add(Convolution2D(10, (4, 4), strides=(1, 1)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(150))
+    model.add(Activation('relu'))
+    model.add(Dense(80))
+    model.add(Activation('relu'))
+    model.add(Dense(60))
+    model.add(Activation('relu'))
+    model.add(Dense(40))
+    model.add(Activation('relu'))
+    model.add(Dense(20))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+
+    return model
+
