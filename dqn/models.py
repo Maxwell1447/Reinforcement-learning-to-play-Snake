33c8226bf329660c5eb91266daa9c1f86e4295f6
Maxime Bouthors <maxbouthors@gmail.com>
2020-01-10 02:15:58 +0100

    trained v3

--- dqn/models.py
+++ dqn/models.py
@@ -13,6 +13,10 @@ def model(input_shape, nb_actions, version: str):
         return model_v3(input_shape, nb_actions)
     elif version == "v4":
         return model_v4(input_shape, nb_actions)
+    elif version == "v5":
+        return model_v4(input_shape, nb_actions)
+    elif version == "v6":
+        return model_v4(input_shape, nb_actions)
     else:
         raise ValueError
 
@@ -152,3 +156,35 @@ def model_v5(input_shape, nb_actions):
 
     return model
 
+
+def model_v6(input_shape, nb_actions):
+
+    model = Sequential()
+    if common.image_dim_ordering() == 'tf':
+        # (width, height, channels)
+        model.add(Permute((2, 3, 1), input_shape=input_shape))
+    elif common.image_dim_ordering() == 'th':
+        # (channels, width, height)
+        model.add(Permute((1, 2, 3), input_shape=input_shape))
+    else:
+        raise RuntimeError('Unknown image_dim_ordering.')
+    model.add(Convolution2D(10, (4, 4), strides=(1, 1)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Convolution2D(30, (3, 3), strides=(1, 1)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Activation('relu'))
+    model.add(Dense(80))
+    model.add(Activation('relu'))
+    model.add(Dense(40))
+    model.add(Activation('relu'))
+    model.add(Dense(20))
+    model.add(Activation('relu'))
+    model.add(Dense(nb_actions))
+    model.add(Activation('linear'))
+    print(model.summary())
+
+    return model
